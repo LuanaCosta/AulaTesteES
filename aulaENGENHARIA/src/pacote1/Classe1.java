4e6a52bb10c55d1716cac05e2cb4267d98fc1d97
tayna.luana <tayna.luana@LAB5-DCX-27.dcx.ufpb.br>
2016-06-07 11:16:02 -0300

    Primeiro commit

---
+++ aulaENGENHARIA/src/pacote1/Classe1.java
@@ -0,0 +1,8 @@
+package pacote1;
+
+public class Classe1 {
+	String aula = "engenharia";
+	String aluno = "Rhaleff";
+	String data="07/06/2016";
+	int nota=10;
+}
---
+++ aulaENGENHARIA/src/pacote2/Classe2.java
@@ -0,0 +1,9 @@
+package pacote2;
+
+public class Classe2 {
+	String aula = "engenharia";
+	String aluno = "Lua";
+	String data="07/06/2016";
+	int nota=10;
+
+}
